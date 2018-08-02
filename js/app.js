678f29b0de5ca1b32851933332c13528ecf7ed44
Earnest Cain <caintre03@gmail.com>
2018-08-02 10:27:53 -0700

    finished all the validation and refactored code so I did not repeat prompt questions

--- js/app.js
+++ js/app.js
@@ -29,9 +29,20 @@ button.addEventListener('click', event => {
       let userAnswer = prompt(el).toLowerCase();
       console.log(userAnswer);
       console.log(typeof userAnswer);
+      let validatingArray = answersCharArray + answersWordArray;
+
+      while (
+        validatingArray.includes(userAnswer) === false ||
+        userAnswer === ''
+      ) {
+        alert('Sorry that is incorrect, please try again');
+        userAnswer = prompt(el).toLowerCase();
+        console.log(userAnswer);
+        console.log(typeof userAnswer);
+      }
       console.log('answer array: ', answersCharArray[index]);
       console.log('user answer: ', userAnswer);
-      if (userAnswer === answersCharArray[index] || userAnswer === answersWordArray[index]) {
+      if (validatingArray.includes(userAnswer) === true) {
         alert(
           'My dawg! ' + visitorsName + ' you know me better than I know myself.'
         );
@@ -48,48 +59,75 @@ button.addEventListener('click', event => {
   //asking number question to user
   let numberQuestion = () => {
     let count = 1;
-    let numberToGuess = parseInt(prompt('Question 6: How many son\'s do I have?'));
-    if (numberToGuess === 2) {
-      alert('Yes, ' + visitorsName + ' you got it right!');
-      numberOfCorrectAnswers++;
-      console.log('correct number of guesses: ', numberOfCorrectAnswers);
-      return;
-    } else {
-      while(count < 5) {
-        numberToGuess = parseInt(prompt('How many son\'s do I have?'));
-        count++;
-        console.log('count variable: ',count);
+    while (count < 5) {
+      let numberToGuess = parseInt(
+        prompt(
+          'Question 6: How many sons do I have? you have five chances to get it correct.'
+        )
+      );
+      if (isNaN(numberToGuess)) {
+        alert('That is not a number. You lose a turn');
+      } else if (numberToGuess === 2) {
+        alert('Yes, ' + visitorsName + ' you got it right!');
+        return;
+      } else if (numberToGuess > 2) {
+        alert('You guessed to high.');
+      } else if (numberToGuess < 2) {
+        alert('You guessed to low.');
+      } else {
+        alert('That is incorrect, please try again');
       }
-      alert('Sorry ' + visitorsName + ' you guessed incorrectly');
+      count++;
+      console.log('count variable: ', count);
     }
+    alert('Sorry, ' + visitorsName + ' you guessed incorrectly');
   };
 
   //asking question with multiple answers to user
   let multipleQuestion = () => {
     let multiArray = ['new york', 'california', 'texas'];
-    let userMultiAnwser = prompt('Ouestion 7: Try to guess one of my favorite states i\'ve visited').toLowerCase();
     let count = 1;
-    while(count < 7) {
-      if (multiArray.includes(userMultiAnwser) === true) {
-        alert(visitorsName + ' You were correct, all possible answers were '
-              + multiArray[0] + ', ' + multiArray[1] + ', ' + multiArray[2] );
+    while (count < 7) {
+      let userMultiAnwser = prompt(
+        'Ouestion 7: Try to guess one of my favorite states i\'ve visited, you have seven chances to get it correct.'
+      ).toLowerCase();
+      if (!isNaN(userMultiAnwser)) {
+        alert('Sorry that is an incorrect value, you lose a turn.');
+      } else if (multiArray.includes(userMultiAnwser) === true) {
+        alert(
+          visitorsName +
+            ' you were correct, all possible answers were ' +
+            multiArray[0] +
+            ', ' +
+            multiArray[1] +
+            ', ' +
+            multiArray[2]
+        );
         numberOfCorrectAnswers++;
         console.log('correct number of guesses: ', numberOfCorrectAnswers);
         return;
       } else {
         alert('Incorrect, try again');
-        userMultiAnwser = prompt('Try to guess one of my favorite states i\'ve visited').toLowerCase();
         count++;
       }
     }
-    alert(visitorsName + ' You were incorrect, all possible answers were '
-          + multiArray[0] + ', ' + multiArray[1] + ', ' + multiArray[2] );
+    alert(
+      visitorsName +
+        ' you were incorrect, all possible answers were ' +
+        multiArray[0] +
+        ', ' +
+        multiArray[1] +
+        ', ' +
+        multiArray[2]
+    );
   };
 
   //displaying the number of correct answer to the user
   const printNumberOfCorrectAnswers = () => {
     console.log('correct number of guesses: ', numberOfCorrectAnswers);
-    alert(visitorsName + ' You scored ' + numberOfCorrectAnswers + ' out of 7 yo!');
+    alert(
+      visitorsName + ' you scored ' + numberOfCorrectAnswers + ' out of 7 yo!'
+    );
   };
 
   //invoking functions
